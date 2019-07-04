afa0c3af795682ad52b664950be56e83a7483361
Yui Ichioka <ichioka.yui@fixer.co.jp>
2019-07-04 18:32:18 +0900

    Add store sample

---
+++ store/index.js
@@ -0,0 +1,24 @@
+// export const strict = false
+
+// export const state = () => ({
+//   target: null
+// })
+
+// export const getters = {
+//   target(state) {
+//     return state.target
+//   }
+// }
+
+// export const mutations = {
+//   setTarget(state, val) {
+//     state.target = val
+//   }
+// }
+
+// export const actions = {
+//   // setTarget({ commit, dispatch, getters, rootGetters, rootState, state }) {
+//   setTarget({ commit }, val) {
+//     commit('setTarget', val)
+//   }
+// }
