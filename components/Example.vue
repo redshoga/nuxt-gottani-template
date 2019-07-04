15fb06bdfdc8586ca2ff94a09dc98d786e7032f5
Yui Ichioka <ichioka.yui@fixer.co.jp>
2019-07-04 18:24:03 +0900

    Add sample components

---
+++ components/Example.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>hello, {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
     <h1>nuxt-gottani-template</h1>
+    <Example name="world" />
   </div>
 </template>
+
+<script>
+import Example from '~/components/Example'
+
+export default {
+  components: {
+    Example
+  }
+}
+</script>
